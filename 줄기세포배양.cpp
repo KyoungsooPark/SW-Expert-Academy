75b94e61d3893b03a4a40606416570722c11c2c7
KyoungsooPark <36439380+KyoungsooPark@users.noreply.github.com>
2018-11-21 23:09:13 +0900

    Update 줄기세포배양.cpp

--- 줄기세포배양.cpp
+++ 줄기세포배양.cpp
@@ -3,68 +3,74 @@ https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId
 */
 
 #include <cstdio>
-#include <cstring>
-#include <queue>
-#define MAXSIZE    150 + 50 + 150
+#define MAX	150 + 50 + 150
+#define QUEUESIZE	700
 using namespace std;
-typedef struct cell { int x, y, life; } cell;
+typedef struct { int x, y, life; } cell;
 
-int board[MAXSIZE][MAXSIZE];
-int dx[4] = { 0, 0, 1, -1 };
-int dy[4] = { 1, -1, 0, 0 };
+cell q[10][QUEUESIZE];	// ìš°ì„ ìˆœìœ„ í
+int map[MAX][MAX];
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+int begin[10], end[10];
+
+int size(int p) { return begin[p] <= end[p] ? end[p] - begin[p] : QUEUESIZE + end[p] - begin[p]; }
+void push(int p, cell c) { q[p][end[p]++] = c; if (end[p] == QUEUESIZE) end[p] = 0; }
+cell pop(int p) { cell ret = q[p][begin[p]++]; if (begin[p] == QUEUESIZE) begin[p] = 0; return ret; }
+
+void reset(void) {
+	// ì„¸í¬ê°€ ì£½ë”ë¼ë„ ì†Œë©¸ë˜ì§€ ì•Šê³  ë‚¨ì•„ìˆìœ¼ë¯€ë¡œ ë¦¬ì…‹í•´ì¤˜ì•¼ í•¨
+	for (int n = 0; n < MAX; n++)
+		for (int m = 0; m < MAX; m++)
+			map[n][m] = 0;
+	// ìš°ì„ ìˆœìœ„ í ì´ˆê¸°í™”
+	for (int p = 0; p < 10; p++)
+		begin[p] = end[p] = 0;
+}
 
 int main(void) {
 	int T;
 	scanf("%d", &T);
 	for (int t = 1; t <= T; t++) {
 		int N, M, K, ans = 0;
-		queue<cell> q[11];
-		// ¼¼Æ÷°¡ Á×´õ¶óµµ ¼Ò¸êµÇÁö ¾Ê°í ³²¾ÆÀÖÀ¸¹Ç·Î ¸®¼ÂÇØÁà¾ß ÇÔ
-		memset(board, 0, sizeof(board));
 
-		// ÀÔ·ÂºÎ
+		// ì…ë ¥ë¶€
 		scanf("%d %d %d", &N, &M, &K);
 		for (int n = 150; n < N + 150; n++) {
 			for (int m = 150; m < M + 150; m++) {
-				scanf("%d", &board[n][m]);
-				if (board[n][m])
-					q[board[n][m]].push({ n, m, board[n][m] * 2 });
+				scanf("%d", &map[n][m]);
+				if (map[n][m])
+					push(map[n][m] - 1, { n, m, map[n][m] * 2 });
 			}
 		}
 
-		// Ã³¸®ºÎ
+		// ì²˜ë¦¬ë¶€
 		while (K--) {
-			// ¿ì¼±¼øÀ§¿¡ µû¶ó (»ı¸í·ÂÀÌ °­ÇÑ ÁÙ±â¼¼Æ÷ºÎÅÍ) ¹ø½Ä
-			for (int l = 10; l > 0; l--) {
-				// Å¥ ³»ÀÇ ÁÙ±â¼¼Æ÷ ¸ğµÎ ¼öÇà
-				int qsize = q[l].size();
-				while (qsize--) {
-					cell c = q[l].front();
-					q[l].pop();
-					// ÁÙ±â¼¼Æ÷°¡ È°¼ºÈ­µÈ °æ¿ì
-					if (c.life == board[c.x][c.y]) {
-						// ³× ¹æÇâÀ¸·Î
-						for (int i = 0; i < 4; i++) {
-							int nx = c.x + dx[i], ny = c.y + dy[i];
-							// ¼¼Æ÷°¡ Á¸ÀçÇÏÁö ¾Ê´Â °÷¿¡ µ¿½Ã¿¡ ¹ø½Ä
-							if (board[nx][ny] == 0) {
-								board[nx][ny] = c.life;
-								q[l].push({ nx, ny, c.life * 2 });
+			for (int p = 9; p >= 0; p--) {	// ìƒëª…ë ¥ì´ ê°•í•œ ì¤„ê¸°ì„¸í¬ë¶€í„° ë²ˆì‹
+				int qsize = size(p);
+				while (qsize--) {	// í ë‚´ì˜ ì¤„ê¸°ì„¸í¬ ëª¨ë‘ ìˆ˜í–‰
+					cell now = pop(p);
+					if (now.life == map[now.x][now.y]) {	// ì¤„ê¸°ì„¸í¬ê°€ í™œì„±í™”ëœ ê²½ìš°
+						for (int d = 0; d < 4; d++) {
+							int nx = now.x + dx[d], ny = now.y + dy[d];
+							if (map[nx][ny] == 0) {	// ë„¤ ë°©í–¥ ì¤‘ ì„¸í¬ê°€ ì¡´ì¬í•˜ì§€ ì•ŠëŠ” ê³³ì— ë²ˆì‹
+								map[nx][ny] = now.life;
+								push(p, { nx, ny, now.life * 2 });
 							}
 						}
 					}
-					// ÁÙ±â¼¼Æ÷´Â ½Ã°£ÀÌ Áö³²¿¡ µû¶ó (K°¡ °¨¼ÒÇÔ¿¡ µû¶ó) »ı¸í °¨¼Ò
-					if (--c.life > 0)
-						// Á×Áö ¾ÊÀº ¼¼Æ÷´Â Å¥¿¡ ´Ù½Ã »ğÀÔ
-						q[l].push(c);
+					if (--now.life)	// ì¤„ê¸°ì„¸í¬ëŠ” ì‹œê°„ì´ ì§€ë‚¨ì— ë”°ë¼ (Kê°€ ê°ì†Œí•¨ì— ë”°ë¼) ìƒëª… ê°ì†Œ
+						push(p, now);	// ì£½ì§€ ì•Šì€ ì„¸í¬ëŠ” íì— ë‹¤ì‹œ ì‚½ì…
 				}
 			}
 		}
 
-		// Ãâ·ÂºÎ
-		for (int l = 1; l <= 10; l++)
-			ans += q[l].size();
+		// ì¶œë ¥ë¶€
+		for (int p = 0; p < 10; p++)
+			ans += size(p);
+
 		printf("#%d %d\n", t, ans);
+		reset();	// ì´ˆê¸°í™”
 	}
 	return 0;
 }
