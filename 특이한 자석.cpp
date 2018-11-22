45e49bdba72b56ee9c950e638bc6113e4684e761
KyoungsooPark <36439380+KyoungsooPark@users.noreply.github.com>
2018-11-22 14:40:38 +0900

    Update 특이한 자석.cpp

--- 특이한 자석.cpp
+++ 특이한 자석.cpp
@@ -3,90 +3,83 @@ https://www.swexpertacademy.com/main/code/problem/problemDetail.do
 */
 
 #include <cstdio>
-#define CLOCKWISE	1
-#define COUNTERCLOCKWISE	-1
 using namespace std;
 
-typedef struct { int wheel_num, direction; } turn;
-turn turn_info[20];
 int wheel[4][8], top_idx[4];
-
-int left_tooth(int wheel_num) {
-	int idx = top_idx[wheel_num] - 2;
+// wheel_idx 번째 톱니바퀴의 왼쪽 자성
+int left_tip(int wheel_idx) {
+	int idx = top_idx[wheel_idx] - 2;
 	if (idx < 0)
 		idx += 8;
-	return wheel[wheel_num][idx];
+	return wheel[wheel_idx][idx];
 }
-
-int right_tooth(int wheel_num) {
-	int idx = top_idx[wheel_num] + 2;
-	if (idx >= 8)
+// wheel_idx 번째 톱니바퀴의 오른쪽 자성
+int right_tip(int wheel_idx) {
+	int idx = top_idx[wheel_idx] + 2;
+	if (idx > 7)
 		idx -= 8;
-	return wheel[wheel_num][idx];
+	return wheel[wheel_idx][idx];
 }
-
-void rotate(int wheel_num, int direction) {
-	if (direction == CLOCKWISE) {
-		top_idx[wheel_num]--;
-		if (top_idx[wheel_num] < 0)
-			top_idx[wheel_num] += 8;
+// wheel_idx 번째 톱니바퀴의 top 위치 변경
+void change_top(int wheel_idx, int direction) {
+	top_idx[wheel_idx] -= direction;
+	if (top_idx[wheel_idx] == -1)
+		top_idx[wheel_idx] = 7;
+	else if (top_idx[wheel_idx] == 8)
+		top_idx[wheel_idx] = 0;
+}
+// 톱니바퀴 회전
+void rotate(int wheel_idx, int direction) {
+	int dir_info[4];
+	dir_info[wheel_idx] = direction;
+	// 왼쪽 톱니바퀴의 회전 방향 설정
+	for (int left = wheel_idx - 1; left >= 0; left--) {
+		if (dir_info[left + 1] && right_tip(left) != left_tip(left + 1))
+			dir_info[left] = dir_info[left + 1] * (-1);
+		else
+			dir_info[left] = 0;
 	}
-	else if (direction == COUNTERCLOCKWISE) {
-		top_idx[wheel_num]++;
-		if (top_idx[wheel_num] >= 8)
-			top_idx[wheel_num] -= 8;
+	// 오른쪽 톱니바퀴의 회전 방향 설정
+	for (int right = wheel_idx + 1; right <= 3; right++) {
+		if (dir_info[right - 1] && left_tip(right) != right_tip(right - 1))
+			dir_info[right] = dir_info[right - 1] * (-1);
+		else
+			dir_info[right] = 0;
 	}
+	// 전체 톱니바퀴 회전
+	for (int wh = 0; wh < 4; wh++)
+		if (dir_info[wh])
+			change_top(wh, dir_info[wh]);
+}
+// 점수 계산
+int score(void) {
+	int ret = 0;
+	for (int wh = 0, s = 1; wh < 4; wh++, s <<= 1) {
+		ret += wheel[wh][top_idx[wh]] * s;
+	}
+	return ret;
 }
 
 int main(void) {
-	int T;
-	scanf("%d", &T);
+	int T; scanf("%d", &T);
 	for (int t = 1; t <= T; t++) {
-		int K, ans = 0;
 		// 입력부
-		scanf("%d", &K);
-		for (int wheel_num = 0; wheel_num < 4; wheel_num++) {
-			for (int tooth_num = 0; tooth_num < 8; tooth_num++)
-				scanf("%1d", &wheel[wheel_num][tooth_num]);
-			top_idx[wheel_num] = 0;
-		}
-		for (int k = 0; k < K; k++) {
-			scanf("%d %d", &turn_info[k].wheel_num, &turn_info[k].direction);
-			turn_info[k].wheel_num--;
+		int K; scanf("%d", &K);
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 8; j++)
+				scanf("%d", &wheel[i][j]);
+			top_idx[i] = 0;
 		}
 
 		// 처리부
-		for (int k = 0; k < K; k++) {
-			int wheel_num = turn_info[k].wheel_num;
-			int direction = turn_info[k].direction;
-			int dir_arr[4];
-			// 전체 톱니바퀴의 회전 방향 설정
-			dir_arr[wheel_num] = direction;
-			// 왼쪽 톱니바퀴의 회전 방향 설정
-			for (int lw = wheel_num - 1; lw >= 0; lw--) {
-				if (dir_arr[lw + 1] != 0 && right_tooth(lw) != left_tooth(lw + 1))
-					dir_arr[lw] = dir_arr[lw + 1] * (-1);
-				else
-					dir_arr[lw] = 0;
-			}
-			// 오른쪽 톱니바퀴의 회전 방향 설정
-			for (int rw = wheel_num + 1; rw < 4; rw++) {
-				if (dir_arr[rw - 1] != 0 && right_tooth(rw - 1) != left_tooth(rw))
-					dir_arr[rw] = dir_arr[rw - 1] * (-1);
-				else
-					dir_arr[rw] = 0;
-			}
-			// 전체 톱니바퀴 회전
-			for (int w = 0; w < 4; w++)
-				rotate(w, dir_arr[w]);
+		while (K--) {
+			int wh, d;
+			scanf("%d %d", &wh, &d);
+			rotate(wh - 1, d);
 		}
 
-		// 출력부
-		for (int w = 0; w < 4; w++) {
-			if (wheel[w][top_idx[w]] == 1)
-				ans += (1 << w);
-		}
-		printf("#%d %d\n", t, ans);
+		//출력부
+		printf("#%d %d\n", t, score());
 	}
 	return 0;
 }
