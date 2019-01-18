0bc61d32b156fb49028fc37458c086cc5adf4731
KyoungsooPark <36439380+KyoungsooPark@users.noreply.github.com>
2019-01-18 14:55:03 +0900

    Add files via upload

---
+++ 부분 수열의 합.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+using namespace std;
+
+int arr[20];
+
+int main(void) {
+	int T;
+	scanf("%d", &T);
+	for (int t = 1; t <= T; t++) {
+		int n, k, ans = 0;
+		// 입력부
+		scanf("%d %d", &n, &k);
+		for (int i = 0; i < n; i++)
+			scanf("%d", &arr[i]);
+		// 처리부
+		for (int i = 1; i < (1 << n); i++) {
+			int temp = 0;
+			for (int j = 0; j < n; j++) {
+				if (i & (1 << j)) {
+					temp += arr[j];
+					if (temp > k)	// 실행 시간을 줄이는 핵심
+						break;
+				}
+			}
+			if (temp == k)
+				ans++;
+		}
+		// 출력부
+		printf("#%d %d\n", t, ans);
+	}
+	return 0;
+}
