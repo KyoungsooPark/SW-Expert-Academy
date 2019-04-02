fffd56b202fa5ffd420e75c13aaa7b3a8403fd8d
KyoungsooPark <ks08_18@hufs.ac.kr>
2019-04-02 18:53:57 +0900

    Update 등산로 조성.cpp

--- 등산로 조성.cpp
+++ 등산로 조성.cpp
@@ -1,31 +1,31 @@
 /*
-https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PoOKKAPIDFAUq&
+https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PoOKKAPIDFAUq&categoryId=AV5PoOKKAPIDFAUq&categoryType=CODE
 */
 
 #include <cstdio>
 using namespace std;
 
-int map[8][8];
 bool visited[8][8];
+int map[8][8];
 int dx[4] = { 0, 0, 1, -1 };
 int dy[4] = { 1, -1, 0, 0 };
-int n, k, ans;
+int N, K, ans;
 
-void go(int x, int y, int h, int l, bool flag) {
-	if (ans < l)
-		ans = l;
+int max(int a, int b) { return a > b ? a : b; }
+
+void go(int x, int y, int n, bool flag) {
 	visited[x][y] = true;
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i], ny = y + dy[i];
-		if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-			if (!visited[nx][ny]) {
-				// 다음 위치에 공사가 필요하지 않은 경우
-				if (map[nx][ny] < h)
-					go(nx, ny, map[nx][ny], l + 1, flag);
-				// 공사가 필요한 경우 이미 공사를 진행하지 않았다면
-				else if (flag == false && map[nx][ny] - k < h)
-					// 현재 위치의 높이보다 1 낮은 만큼만 깎고 flag를 true로 설정
-					go(nx, ny, h - 1, l + 1, true);
+	ans = max(ans, n);
+	for (int d = 0; d < 4; d++) {
+		int nx = x + dx[d], ny = y + dy[d];
+		if (0 <= nx && nx < N && 0 <= ny && ny < N && !visited[nx][ny]) {
+			if (map[nx][ny] < map[x][y])	// 더 낮은 지형인 경우
+				go(nx, ny, n + 1, flag);
+			else if (!flag && map[nx][ny] - map[x][y] < K) {	// 깎아서 이동 가능한 경우
+				int temp = map[nx][ny];	// 깎기 전 높이
+				map[nx][ny] = map[x][y] - 1;	// 깎은 후 높이
+				go(nx, ny, n + 1, true);
+				map[nx][ny] = temp;
 			}
 		}
 	}
@@ -36,27 +36,20 @@ int main(void) {
 	int T;
 	scanf("%d", &T);
 	for (int t = 1; t <= T; t++) {
-		int peak = 0;
-
+		int M = 0;
 		// 입력부
-		scanf("%d %d", &n, &k);
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
+		scanf("%d %d", &N, &K);
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++) {
 				scanf("%d", &map[i][j]);
-				if (peak < map[i][j])
-					peak = map[i][j];
+				M = max(M, map[i][j]);
 			}
-		}
-
 		// 처리부
-		ans = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (map[i][j] == peak)
-					go(i, j, map[i][j], 1, false);
-			}
-		}
-
+		ans = 1;
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				if (map[i][j] == M)
+					go(i, j, 1, false);
 		// 출력부
 		printf("#%d %d\n", t, ans);
 	}
